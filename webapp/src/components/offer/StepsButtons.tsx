30c414bb2655eb91b967d02e510c1a2cd3066e00
horek <azenoxe@gmail.com>
2024-04-04 11:51:25 +0200

    fix: coupon guard for multiple assign to one user issue

--- webapp/src/components/offer/StepsButtons.tsx
+++ webapp/src/components/offer/StepsButtons.tsx
@@ -1,71 +1,102 @@
 import { Button, VStack } from "@chakra-ui/react";
 import { push } from "@socialgouv/matomo-next";
+import { useState } from "react";
 import { OfferIncluded } from "~/server/api/routers/offer";
 
 type StepsButtonsProps = {
-	mainBtnText: string;
-	activeStep: number;
-	count: number;
-	offer: OfferIncluded;
-	setActiveStep: (index: number) => void;
-	handleValidate?: () => void;
-	onClose: () => void;
+  mainBtnText: string;
+  activeStep: number;
+  count: number;
+  offer: OfferIncluded;
+  setActiveStep: (index: number) => void;
+  handleValidate?: () => Promise<void>;
+  isValidateSuccess?: boolean;
+  onClose: () => void;
 };
 
 const StepsButtons = ({
-	mainBtnText,
-	handleValidate,
-	onClose,
-	activeStep,
-	setActiveStep,
-	offer,
-	count,
+  mainBtnText,
+  handleValidate,
+  isValidateSuccess,
+  onClose,
+  activeStep,
+  setActiveStep,
+  offer,
+  count,
 }: StepsButtonsProps) => {
-	const handleOnClose = () => {
-		setActiveStep(1);
-		onClose();
-	};
+  const [isValidating, setIsValidating] = useState(false);
 
-	return (
-		<VStack
-			spacing={6}
-			bottom={0}
-			pb={6}
-			position={"fixed"}
-			w="full"
-			left={0}
-			px={4}
-			bg={"bgWhite"}
-			pt={4}
-		>
-			<Button
-				onClick={() => {
-					if (activeStep !== count) {
-						push(['trackEvent', 'Offre', `${offer.partner.name}`, `${offer.title}`, 'Inactive', "J'active mon offre", "J'ai compris"])
-						setActiveStep(activeStep + 1)
-					} else if (handleValidate) {
-						push(['trackEvent', 'Offre', `${offer.partner.name}`, `${offer.title}`, "Inactive", "J'active mon offre - Validation"])
-						handleValidate()
-					}
-				}}
-				w="full"
-			>
-				{activeStep !== count ? "J'ai compris" : mainBtnText}
-			</Button>
-			<Button
-				onClick={() => {
-					push(['trackEvent', 'Offre', `${offer.partner.name}`, `${offer.title}`, "Inactive", "J'active mon offre", `Cette offre ne m'intéresse pas ${activeStep === count ? '2' : ''}`])
-					handleOnClose()
-				}}
-				variant="ghost"
-				colorScheme="btnWhite"
-				size="md"
-				w="full"
-			>
-				Cette offre ne m'interesse pas
-			</Button>
-		</VStack>
-	);
+  const handleOnClose = () => {
+    setActiveStep(1);
+    onClose();
+  };
+
+  return (
+    <VStack
+      spacing={6}
+      bottom={0}
+      pb={6}
+      position={"fixed"}
+      w="full"
+      left={0}
+      px={4}
+      bg={"bgWhite"}
+      pt={4}
+    >
+      <Button
+        onClick={() => {
+          if (activeStep !== count) {
+            push([
+              "trackEvent",
+              "Offre",
+              `${offer.partner.name}`,
+              `${offer.title}`,
+              "Inactive",
+              "J'active mon offre",
+              "J'ai compris",
+            ]);
+            setActiveStep(activeStep + 1);
+          } else if (handleValidate) {
+            push([
+              "trackEvent",
+              "Offre",
+              `${offer.partner.name}`,
+              `${offer.title}`,
+              "Inactive",
+              "J'active mon offre - Validation",
+            ]);
+            setIsValidating(true);
+            handleValidate().then(() => setIsValidating(false));
+          }
+        }}
+        isLoading={isValidating}
+        isDisabled={isValidateSuccess}
+        w="full"
+      >
+        {activeStep !== count ? "J'ai compris" : mainBtnText}
+      </Button>
+      <Button
+        onClick={() => {
+          push([
+            "trackEvent",
+            "Offre",
+            `${offer.partner.name}`,
+            `${offer.title}`,
+            "Inactive",
+            "J'active mon offre",
+            `Cette offre ne m'intéresse pas ${activeStep === count ? "2" : ""}`,
+          ]);
+          handleOnClose();
+        }}
+        variant="ghost"
+        colorScheme="btnWhite"
+        size="md"
+        w="full"
+      >
+        Cette offre ne m'interesse pas
+      </Button>
+    </VStack>
+  );
 };
 
 export default StepsButtons;
