47d3815d62fd1c2185330a1cec2b21cf3cbf1063
horek <azenoxe@gmail.com>
2024-04-08 16:43:30 +0200

    fix: add payload migrations

---
+++ webapp/src/payload/migrations/20240408_144308.ts
@@ -0,0 +1,24 @@
+import { MigrateUpArgs, MigrateDownArgs } from '@payloadcms/db-postgres'
+import { sql } from 'drizzle-orm'
+
+export async function up({ payload }: MigrateUpArgs): Promise<void> {
+await payload.db.drizzle.execute(sql`
+
+DO $$ BEGIN
+ CREATE TYPE "enum_users_notification_status" AS ENUM('enabled', 'disabled');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+ALTER TABLE "users" ADD COLUMN "notification_status" "enum_users_notification_status";
+ALTER TABLE "users" ADD COLUMN "notification_subscription" jsonb;`);
+
+};
+
+export async function down({ payload }: MigrateDownArgs): Promise<void> {
+await payload.db.drizzle.execute(sql`
+
+ALTER TABLE "users" DROP COLUMN IF EXISTS "notification_status";
+ALTER TABLE "users" DROP COLUMN IF EXISTS "notification_subscription";`);
+
+};
