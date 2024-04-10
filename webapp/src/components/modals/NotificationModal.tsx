2bb7ec70e0b515c8c59a5ae2eb6ccc098681fc19
horek <azenoxe@gmail.com>
2024-04-10 15:40:11 +0200

    fix: add page to handle user notification on account page

--- webapp/src/components/modals/NotificationModal.tsx
+++ webapp/src/components/modals/NotificationModal.tsx
@@ -4,19 +4,7 @@ import StackItems from "../offer/StackItems";
 import { useAuth } from "~/providers/Auth";
 import { useEffect, useState } from "react";
 import { api } from "~/utils/api";
-
-const base64ToUint8Array = (base64: string) => {
-  const padding = "=".repeat((4 - (base64.length % 4)) % 4);
-  const b64 = (base64 + padding).replace(/-/g, "+").replace(/_/g, "/");
-
-  const rawData = window.atob(b64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-};
+import { base64ToUint8Array } from "~/utils/tools";
 
 const NotificationModal = ({
   onClose,
