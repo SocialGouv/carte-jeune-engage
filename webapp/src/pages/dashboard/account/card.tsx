67ff6b7e669e4f9678d5f1a28bc049ea9cc54139
horek <azenoxe@gmail.com>
2024-04-25 16:50:46 +0200

    feat: handle different card status on card page with new layout and remove old logic on account index page

--- webapp/src/pages/dashboard/account/card.tsx
+++ webapp/src/pages/dashboard/account/card.tsx
@@ -1,13 +1,40 @@
-import { Box, Center, Flex, Heading, Icon, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  Heading,
+  Icon,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useAuth } from "~/providers/Auth";
 import LoadingLoader from "~/components/LoadingLoader";
-import { HiArrowLeft, HiCheckBadge } from "react-icons/hi2";
 import Image from "next/image";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+import { useMemo } from "react";
+import { HiCheckBadge, HiShieldCheck } from "react-icons/hi2";
+import NewPassComponent from "~/components/NewPassComponent";
 
 export default function AccountCard() {
   const router = useRouter();
   const { user } = useAuth();
+
+  const {
+    isOpen: isOpenNewPassComponent,
+    onOpen: onOpenNewPassComponent,
+    onClose: onCloseNewPassComponent,
+  } = useDisclosure();
+
+  const passCJEStatus = useMemo(() => {
+    return user?.status_image === "pending" && user.image
+      ? "pending"
+      : !user?.image
+      ? "missing"
+      : undefined;
+  }, [user]);
 
   if (!user)
     return (
@@ -18,22 +45,69 @@ export default function AccountCard() {
 
   return (
     <Box pt={12} pb={36} px={8}>
-      <Icon
-        as={HiArrowLeft}
-        w={6}
-        h={6}
-        onClick={() => router.back()}
-        cursor="pointer"
-      />
-      <Heading
-        as="h2"
-        size="lg"
-        fontWeight="extrabold"
-        mt={4}
-        textAlign="center"
-      >
-        Ma carte CJE
-      </Heading>
+      <Flex position="relative" justifyContent="center">
+        <Button
+          colorScheme="whiteBtn"
+          onClick={() => router.back()}
+          pos="absolute"
+          left={0}
+          size="md"
+          width={8}
+          iconSpacing={0}
+          px={0}
+          rightIcon={<ChevronLeftIcon w={6} h={6} color="black" />}
+        />
+        <Heading
+          as="h2"
+          size="lg"
+          fontWeight="extrabold"
+          textAlign="center"
+          my={1}
+        >
+          Ma carte CJE
+        </Heading>
+      </Flex>
+      {passCJEStatus && (
+        <Center
+          display="flex"
+          flexDir="column"
+          textAlign="center"
+          border="1px solid"
+          borderRadius="1.5xl"
+          borderColor="borderGray"
+          mt={6}
+          p={6}
+        >
+          <Flex alignItems="center" gap={2}>
+            <Icon
+              as={passCJEStatus === "pending" ? HiShieldCheck : HiCheckBadge}
+              w={6}
+              h={6}
+              color="primary.500"
+            />
+            <Text fontWeight="bold" color="primary.500">
+              {passCJEStatus === "pending"
+                ? "Photo en cours de vérification"
+                : "Validez votre carte CJE"}
+            </Text>
+          </Flex>
+          <Text fontWeight="medium" mt={2}>
+            {passCJEStatus === "pending"
+              ? "Nous avons bien reçu votre photo, si votre visage est correctement visible, en 24h c’est bon !"
+              : "Il manque votre photo pour valider votre carte"}
+          </Text>
+          {passCJEStatus === "missing" && (
+            <Button mt={3} size="lg" w="full" onClick={onOpenNewPassComponent}>
+              <Box lineHeight="short">
+                Ajouter ma photo
+                <Text opacity="70%" fontSize="xs">
+                  obligatoire
+                </Text>
+              </Box>
+            </Button>
+          )}
+        </Center>
+      )}
       <Flex
         flexDir="column"
         alignItems="center"
@@ -41,39 +115,48 @@ export default function AccountCard() {
         borderRadius="1.5xl"
         pt={12}
         pb={6}
-        mt={10}
+        mt={passCJEStatus ? 6 : 10}
         gap={8}
       >
         <Box borderRadius="full" overflow="hidden">
-          <Image
-            src={user.image.url as string}
-            alt={user.image.alt as string}
-            width={111}
-            height={111}
-            objectFit="cover"
-            objectPosition="center"
-            style={{ width: "111px", height: "111px" }}
-          />
+          {user.status_image === "approved" ? (
+            <Image
+              src={user.image.url as string}
+              alt={user.image.alt as string}
+              width={111}
+              height={111}
+              objectFit="cover"
+              objectPosition="center"
+              style={{ width: "111px", height: "111px" }}
+            />
+          ) : (
+            <Box
+              w={111}
+              h={111}
+              bgColor="cje-gray.500"
+              borderRadius="full"
+              justifyContent="center"
+              alignItems="center"
+            />
+          )}
         </Box>
-        <Flex flexDir="column" alignItems="center" gap={3}>
-          <Text fontSize="2xl" fontWeight="extrabold">
-            {user.firstName} {user.lastName}
-          </Text>
-          <Text fontSize="sm" fontWeight="medium">
-            ID {user.id}
-          </Text>
-        </Flex>
-        <Flex
-          alignItems="center"
-          bgColor="primary.500"
-          borderRadius="xl"
-          px={3}
-          py={2}
-        >
-          <Icon as={HiCheckBadge} w={5} h={5} fill="white" mr={2} />
-          <Text fontSize="sm" fontWeight="bold" color="white">
-            Carte vérifiée
-          </Text>
+        <Flex flexDir="column" gap={4}>
+          <Flex flexDir="column" alignItems="center">
+            <Text fontWeight="medium" fontSize="xs" color="disabled">
+              Nom
+            </Text>
+            <Text fontSize="2xl" fontWeight="extrabold">
+              {user.firstName} {user.lastName}
+            </Text>
+          </Flex>
+          <Flex flexDir="column" alignItems="center">
+            <Text fontWeight="medium" fontSize="xs" color="disabled">
+              ID
+            </Text>
+            <Text fontSize="xl" fontWeight="extrabold">
+              {user.id}
+            </Text>
+          </Flex>
         </Flex>
         <Flex flexDir="column" alignItems="center" gap={3}>
           <Image
@@ -90,10 +173,20 @@ export default function AccountCard() {
           >
             Carte membre beta testeur 2024
             <br />
-            Projet carte jeunes engagés
+            Projet carte "jeunes engagés"
           </Text>
         </Flex>
       </Flex>
+      <Divider my={6} borderColor="borderGray" />
+      <Text fontSize="sm" fontWeight="bold">
+        La carte CJE est utile pour les offres et les réductions en magasin 🛒
+        <br />
+        Vous pouvez la présenter au commerçant au moment de payer, en caisse.
+      </Text>
+      <NewPassComponent
+        isOpen={isOpenNewPassComponent}
+        onClose={onCloseNewPassComponent}
+      />
     </Box>
   );
 }
--- webapp/src/pages/dashboard/account/index.tsx
+++ webapp/src/pages/dashboard/account/index.tsx
@@ -2,18 +2,14 @@ import { Box, Button, Center, Flex, Icon, Text } from "@chakra-ui/react";
 import { deleteCookie } from "cookies-next";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { User } from "~/payload/payload-types";
 import { useMemo, useState } from "react";
 import {
   HiBuildingLibrary,
   HiChatBubbleOvalLeftEllipsis,
   HiCurrencyEuro,
-  HiExclamationTriangle,
-  HiCheckBadge,
   HiMiniChevronRight,
   HiMiniPower,
   HiUser,
-  HiShieldCheck,
   HiBookOpen,
   HiMiniShieldCheck,
   HiBell,
@@ -31,49 +27,6 @@ import { isIOS } from "~/utils/tools";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 
 const CRISP_TOKEN = process.env.NEXT_PUBLIC_CRISP_TOKEN as string;
-
-const displayDynamicCJECardMessage = (user: User) => {
-  if (!user.image) {
-    return (
-      <>
-        <Flex alignItems="center" color="error">
-          <Icon as={HiExclamationTriangle} w={5} h={5} mr={1.5} />
-          <Text fontSize="xs" fontWeight="bold">
-            Photo manquante
-          </Text>
-        </Flex>
-        <Text fontSize="sm" fontWeight="medium">
-          Finalisez votre carte CJE pour profiter des offres en magasin
-        </Text>
-      </>
-    );
-  } else if (user.image && user.status_image === "pending") {
-    return (
-      <>
-        <Flex alignItems="center" color="primary.500">
-          <Icon as={HiShieldCheck} w={5} h={5} mr={1.5} />
-          <Text fontSize="xs" fontWeight="bold">
-            Notre équipe est en train de créer votre carte...
-          </Text>
-        </Flex>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <Text fontSize="sm" fontWeight="medium">
-        À présenter pour bénéficier des réductions en magasin
-      </Text>
-      <Flex alignItems="center" color="primary.500">
-        <Icon as={HiCheckBadge} w={5} h={5} mr={1.5} />
-        <Text fontSize="xs" fontWeight="bold">
-          Carte vérifiée
-        </Text>
-      </Flex>
-    </>
-  );
-};
 
 export default function Account() {
   const router = useRouter();
@@ -245,19 +198,7 @@ export default function Account() {
                 <Text fontWeight="bold" noOfLines={1}>
                   {item.label}
                 </Text>
-                {item.slug === "card" && displayDynamicCJECardMessage(user)}
               </Flex>
-              {item.slug === "card" && user.status_image !== "approved" && (
-                <Box w={8} mt={3}>
-                  <Box
-                    borderRadius="full"
-                    w={2}
-                    h={2}
-                    bgColor={!user.image ? "error" : "primary.500"}
-                    ml="auto"
-                  />
-                </Box>
-              )}
             </Flex>
           </Link>
         ))}
