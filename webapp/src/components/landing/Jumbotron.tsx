487e5d9f5e48cc1dfb107b215575d1b47b03576f
dan1M <maneadaniel555@gmail.com>
2024-10-21 17:17:06 +0200

    feat: jumbotron image orbit animation + control to play/pause (disabled on mobile)

--- webapp/src/components/landing/Jumbotron.tsx
+++ webapp/src/components/landing/Jumbotron.tsx
@@ -5,12 +5,18 @@ import {
   Heading,
   useBreakpointValue,
   Image,
+  Button,
+  useBoolean,
+  Icon,
 } from "@chakra-ui/react";
 import EllipsePositionnedImages from "./EllipsePositionnedImages";
 import { useRef } from "react";
+import { HiPause, HiPlay } from "react-icons/hi2";
 
 const Jumbotron = () => {
   const firstSectionRef = useRef<HTMLDivElement>(null);
+
+  const [shouldAnimate, { toggle: toggleShouldAnimate }] = useBoolean(true);
 
   const ellipseImages =
     useBreakpointValue({
@@ -54,7 +60,7 @@ const Jumbotron = () => {
         width={{ base: "116px", lg: "176px" }}
         height={{ base: "64px", lg: "94px" }}
       />
-      <Box w={"80%"}>
+      <Flex flexDir={"column"} w={"80%"} alignItems={"center"}>
         <Heading fontSize={{ base: "2xl", lg: "5xl" }} fontWeight="extrabold">
           Des avantages pour les jeunes
           <Box as="br" display={{ base: "none", lg: "block" }} /> qui s’engagent
@@ -68,10 +74,31 @@ const Jumbotron = () => {
         >
           Dispositif en cours d’expérimentation
         </Text>
-      </Box>
+        <Button
+          display={{ base: "none", lg: "flex" }}
+          w={"fit-content"}
+          h={"fit-content"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          onClick={toggleShouldAnimate}
+          variant={"unstyled"}
+          color="disabled"
+          _hover={{ bg: "gray.100" }}
+          p={2}
+          mt={2}
+          lineHeight={0}
+          fontWeight={"extrabold"}
+        >
+          <Icon as={shouldAnimate ? HiPause : HiPlay} mr={2} />
+          <Text as="span" fontSize={{ base: "xs", lg: "sm" }}>
+            Animation
+          </Text>
+        </Button>
+      </Flex>
       <EllipsePositionnedImages
         images={ellipseImages}
         parentRef={firstSectionRef}
+        isPlayingAnimation={shouldAnimate}
       />
     </Flex>
   );
