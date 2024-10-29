d21cbc3602e30ceebdbd2f12da1d525f5f9d2c3d
Clément <clement@numericite.eu>
2024-10-29 18:08:49 +0100

    fix: datetime to french timezone

--- scripts/obiz/config/utils.py
+++ scripts/obiz/config/utils.py
@@ -1,10 +1,11 @@
-from datetime import datetime
 import pytz
+from datetime import datetime
 
 
 def convert_date_format(date_str: str) -> str:
     """
-    Convertit une date du format "DD/MM/YYYY HH:MM:SS" vers le format ISO "YYYY-MM-DDThh:mm:ss.000Z"
+    Convertit une date du format "DD/MM/YYYY HH:MM:SS" (Paris timezone)
+    vers le format ISO "YYYY-MM-DDThh:mm:ss.000Z" (UTC)
 
     Args:
         date_str (str): Date au format "DD/MM/YYYY HH:MM:SS"
@@ -16,9 +17,13 @@ def convert_date_format(date_str: str) -> str:
         if date_str == '':
             return '1970-01-01T00:00:00.000Z'
 
-        date_obj = datetime.strptime(date_str, "%d/%m/%Y %H:%M:%S")
+        naive_date = datetime.strptime(date_str, "%d/%m/%Y %H:%M:%S")
 
-        utc_date = pytz.utc.localize(date_obj)
+        paris_tz = pytz.timezone('Europe/Paris')
+
+        paris_date = paris_tz.localize(naive_date)
+
+        utc_date = paris_date.astimezone(pytz.UTC)
 
         iso_date = utc_date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
