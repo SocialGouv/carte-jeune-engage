4259929d75ba32f49365c076c132001fb6bcafe9
Clément <clement@numericite.eu>
2024-11-04 17:35:54 +0100

    feat: begin obiz offer page

--- packages/obiz/importer.py
+++ packages/obiz/importer.py
@@ -69,13 +69,13 @@ class DataImporter:
         partner_name = sousgenre.get('sousgenres_nom', '')
 
         if len(reduction_percentages) == 0:
-            offer['title'] = f"Offre chez {partner_name}"
+            offer['title'] = f"Offre"
         elif len(reduction_percentages) == 1:
-            offer['title'] = f"-{int(reduction_percentages[0])}% chez {partner_name}"
+            offer['title'] = f"-{int(reduction_percentages[0])}%"
         else:
             min_reduction = int(min(reduction_percentages))
             max_reduction = int(max(reduction_percentages))
-            offer['title'] = f"Entre -{min_reduction}% et -{max_reduction}% chez {partner_name}"
+            offer['title'] = f"Entre -{min_reduction}% et -{max_reduction}%"
 
         offer['formatedTitle'] = offer['title']
 
--- webapp/src/pages/dashboard/offer/obiz/[id]/index.tsx
+++ webapp/src/pages/dashboard/offer/obiz/[id]/index.tsx
@@ -1,6 +1,9 @@
-import { Box, Center } from "@chakra-ui/react";
+import { Button, Center, Flex, Icon, Tag, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { BarcodeIcon } from "~/components/icons/barcode";
 import LoadingLoader from "~/components/LoadingLoader";
+import BackButton from "~/components/ui/BackButton";
+import Image from "~/components/ui/Image";
 import { api } from "~/utils/api";
 
 export default function OfferObizPage() {
@@ -31,5 +34,55 @@ export default function OfferObizPage() {
     return;
   }
 
-  return <Box>{JSON.stringify(resultOffer, null, 2)}</Box>;
+  return (
+    <Flex direction={"column"}>
+      <Flex
+        bg={offer.partner.color}
+        px={8}
+        pt={12}
+        pb={10}
+        direction={"column"}
+        gap={6}
+      >
+        <BackButton />
+        <Flex
+          alignItems={"center"}
+          direction={"column"}
+          bg="white"
+          py={8}
+          px={4}
+          rounded={"2xl"}
+          gap={3}
+          mx={4}
+        >
+          <Flex direction={"column"} mb={2} alignItems={"center"} gap={2}>
+            <Image
+              src={offer.partner.icon.url || ""}
+              width={60}
+              height={offer.partner.icon.height || 50}
+              alt={`Logo ${offer.partner.name}`}
+            />
+            <Text fontWeight={700} fontSize={"xl"}>
+              {offer.partner.name}
+            </Text>
+          </Flex>
+          <Tag fontWeight={700}>
+            <BarcodeIcon mr={1} /> Bon d'achat
+          </Tag>
+          <Text fontWeight={800} fontSize="3xl" textAlign={"center"}>
+            {offer.title}
+          </Text>
+        </Flex>
+      </Flex>
+
+      <Flex direction={"column"} p={8} gap={4}>
+        <Text textAlign={"center"} fontSize={"2xl"} fontWeight={700}>
+          {offer.partner.name}
+        </Text>
+        <Button colorScheme="blackBtn" mt={4}>
+          Acheter un bon
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
