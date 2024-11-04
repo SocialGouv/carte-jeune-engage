48815804aeaab07f83ef172355252537cec16e48
Clément <clement@numericite.eu>
2024-11-04 15:08:24 +0100

    fix: add source type in route to offer

--- webapp/src/components/wrappers/SearchWrapper.tsx
+++ webapp/src/components/wrappers/SearchWrapper.tsx
@@ -12,105 +12,105 @@ import LoadingLoader from "../LoadingLoader";
 import { PartnerIncluded } from "~/server/api/routers/partner";
 
 type SearchWrapperProps = {
-  onSearchChange?: (search: string) => void;
-  fromWidget?: boolean;
-  children: (
-    paginatedTags: TagIncluded[][],
-    partners: (PartnerIncluded & { link: string })[],
-    debouncedSearch: string
-  ) => React.ReactNode;
+	onSearchChange?: (search: string) => void;
+	fromWidget?: boolean;
+	children: (
+		paginatedTags: TagIncluded[][],
+		partners: (PartnerIncluded & { link: string })[],
+		debouncedSearch: string
+	) => React.ReactNode;
 };
 
 const SearchWrapper = ({
-  children,
-  onSearchChange,
-  fromWidget,
+	children,
+	onSearchChange,
+	fromWidget,
 }: SearchWrapperProps) => {
-  const router = useRouter();
-  const [search, setSearch] = useState("");
-  const [debouncedSearch] = useDebounceValue(search, 500);
-  const isSearchDebounced = debouncedSearch !== search;
+	const router = useRouter();
+	const [search, setSearch] = useState("");
+	const [debouncedSearch] = useDebounceValue(search, 500);
+	const isSearchDebounced = debouncedSearch !== search;
 
-  const { data: resultTags, isLoading: isLoadingTags } =
-    api.globals.tagsListOrdered.useQuery({
-      search: debouncedSearch,
-    });
+	const { data: resultTags, isLoading: isLoadingTags } =
+		api.globals.tagsListOrdered.useQuery({
+			search: debouncedSearch,
+		});
 
-  const { data: resultOffers, isLoading: isLoadingOffers } = fromWidget
-    ? api.offer.getWidgetListOfAvailables.useQuery({
-        page: 1,
-        perPage: 8,
-        searchOnPartner: debouncedSearch,
-      })
-    : api.offer.getListOfAvailables.useQuery({
-        page: 1,
-        perPage: 8,
-        searchOnPartner: debouncedSearch,
-      });
+	const { data: resultOffers, isLoading: isLoadingOffers } = fromWidget
+		? api.offer.getWidgetListOfAvailables.useQuery({
+			page: 1,
+			perPage: 8,
+			searchOnPartner: debouncedSearch,
+		})
+		: api.offer.getListOfAvailables.useQuery({
+			page: 1,
+			perPage: 8,
+			searchOnPartner: debouncedSearch,
+		});
 
-  const { data: tags } = resultTags || { data: [] };
-  const { data: offers } = resultOffers || { data: [] };
+	const { data: tags } = resultTags || { data: [] };
+	const { data: offers } = resultOffers || { data: [] };
 
-  const partners = offers.map((offer) => ({
-    ...offer.partner,
-    link: fromWidget
-      ? `/widget/offer/${offer.id}`
-      : `/dashboard/offer/${offer.id}`,
-  }));
+	const partners = offers.map((offer) => ({
+		...offer.partner,
+		link: fromWidget
+			? `/widget/offer/${offer.id}`
+			: `/dashboard/offer/cje/${offer.id}`,
+	}));
 
-  const paginatedTags = paginateArray(tags, 6);
+	const paginatedTags = paginateArray(tags, 6);
 
-  useEffect(() => {
-    if (onSearchChange) onSearchChange(debouncedSearch);
-  }, [debouncedSearch]);
+	useEffect(() => {
+		if (onSearchChange) onSearchChange(debouncedSearch);
+	}, [debouncedSearch]);
 
-  const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-      <Box pt={8} pb={32} minH="full">
-        <Flex
-          alignItems="center"
-          gap={4}
-          px={8}
-          pb={4}
-          borderBottomWidth={1}
-          borderBottomColor="cje-gray.400"
-          mb={6}
-        >
-          <IconButton
-            variant="unstyled"
-            mt={1}
-            icon={<Icon as={HiChevronLeft} w={6} h={6} />}
-            aria-label="Retour à l'accueil"
-            onClick={() => {
-              if (fromWidget) {
-                router.push("/widget");
-              } else {
-                router.push("/dashboard");
-              }
-            }}
-          />
-          <SearchBar
-            search={search}
-            setSearch={setSearch}
-            inputProps={{ autoFocus: true }}
-            placeholder={fromWidget ? "Rechercher" : undefined}
-          />
-        </Flex>
-        {children}
-      </Box>
-    );
-  };
+	const Layout = ({ children }: { children: React.ReactNode }) => {
+		return (
+			<Box pt={8} pb={32} minH="full">
+				<Flex
+					alignItems="center"
+					gap={4}
+					px={8}
+					pb={4}
+					borderBottomWidth={1}
+					borderBottomColor="cje-gray.400"
+					mb={6}
+				>
+					<IconButton
+						variant="unstyled"
+						mt={1}
+						icon={<Icon as={HiChevronLeft} w={6} h={6} />}
+						aria-label="Retour à l'accueil"
+						onClick={() => {
+							if (fromWidget) {
+								router.push("/widget");
+							} else {
+								router.push("/dashboard");
+							}
+						}}
+					/>
+					<SearchBar
+						search={search}
+						setSearch={setSearch}
+						inputProps={{ autoFocus: true }}
+						placeholder={fromWidget ? "Rechercher" : undefined}
+					/>
+				</Flex>
+				{children}
+			</Box>
+		);
+	};
 
-  if (isLoadingTags || isLoadingOffers || isSearchDebounced)
-    return (
-      <Layout>
-        <Center h="full" w="full">
-          <LoadingLoader />
-        </Center>
-      </Layout>
-    );
+	if (isLoadingTags || isLoadingOffers || isSearchDebounced)
+		return (
+			<Layout>
+				<Center h="full" w="full">
+					<LoadingLoader />
+				</Center>
+			</Layout>
+		);
 
-  return <Layout>{children(paginatedTags, partners, debouncedSearch)}</Layout>;
+	return <Layout>{children(paginatedTags, partners, debouncedSearch)}</Layout>;
 };
 
 export default SearchWrapper;
--- webapp/src/notifications/reminderOfferUserPreferences.ts
+++ webapp/src/notifications/reminderOfferUserPreferences.ts
@@ -6,128 +6,128 @@ import { getBaseUrl, payloadWhereOfferIsValid } from "../utils/tools";
 const slug = "reminder-offer-user-preferences";
 
 export async function sendReminderOfferUserPreferences() {
-  console.log(`[${slug}] - Start sending notifications`);
+	console.log(`[${slug}] - Start sending notifications`);
 
-  let nbOfNotificationsSent = 0;
-  let nbOfNotificationsInDb = 0;
+	let nbOfNotificationsSent = 0;
+	let nbOfNotificationsInDb = 0;
 
-  // Only send notification on Wednesday
-  // const today = new Date();
-  // if (today.getDay() !== 3) {
-  //   console.log(`[${slug}] - Not Wednesday`);
-  //   return;
-  // }
+	// Only send notification on Wednesday
+	// const today = new Date();
+	// if (today.getDay() !== 3) {
+	//   console.log(`[${slug}] - Not Wednesday`);
+	//   return;
+	// }
 
-  try {
-    const payload = await getPayloadClient({ seed: false });
+	try {
+		const payload = await getPayloadClient({ seed: false });
 
-    const users = await payload.find({
-      collection: "users",
-      pagination: false,
-      depth: 0,
-      where: {
-        notification_status: {
-          equals: "enabled",
-          exists: true,
-        },
-        notification_subscription: {
-          exists: true,
-        },
-        preferences: {
-          exists: true,
-        },
-      },
-    });
+		const users = await payload.find({
+			collection: "users",
+			pagination: false,
+			depth: 0,
+			where: {
+				notification_status: {
+					equals: "enabled",
+					exists: true,
+				},
+				notification_subscription: {
+					exists: true,
+				},
+				preferences: {
+					exists: true,
+				},
+			},
+		});
 
-    for (const user of users.docs) {
-      let offers = await payload.find({
-        collection: "offers",
-        pagination: false,
-        depth: 1,
-        where: {
-          category: {
-            in: user.preferences,
-          },
-          ...payloadWhereOfferIsValid(),
-        },
-      });
+		for (const user of users.docs) {
+			let offers = await payload.find({
+				collection: "offers",
+				pagination: false,
+				depth: 1,
+				where: {
+					category: {
+						in: user.preferences,
+					},
+					...payloadWhereOfferIsValid(),
+				},
+			});
 
-      await Promise.all(
-        offers.docs.map(async (offer) => {
-          const offerCoupons = await payload.find({
-            collection: "coupons",
-            limit: 1,
-            where: {
-              offer: {
-                equals: offer.id,
-              },
-              user: {
-                exists: false,
-              },
-              used: {
-                equals: false,
-              },
-            },
-          });
+			await Promise.all(
+				offers.docs.map(async (offer) => {
+					const offerCoupons = await payload.find({
+						collection: "coupons",
+						limit: 1,
+						where: {
+							offer: {
+								equals: offer.id,
+							},
+							user: {
+								exists: false,
+							},
+							used: {
+								equals: false,
+							},
+						},
+					});
 
-          const userCoupons = await payload.find({
-            collection: "coupons",
-            limit: 1,
-            where: {
-              offer: {
-                equals: offer.id,
-              },
-              user: {
-                equals: user.id,
-              },
-              used: {
-                equals: false,
-              },
-            },
-          });
+					const userCoupons = await payload.find({
+						collection: "coupons",
+						limit: 1,
+						where: {
+							offer: {
+								equals: offer.id,
+							},
+							user: {
+								equals: user.id,
+							},
+							used: {
+								equals: false,
+							},
+						},
+					});
 
-          return offerCoupons.docs.length === 0 || userCoupons.docs.length > 0;
-        })
-      ).then((results) => {
-        offers.docs = offers.docs.filter((_, index) => results[index]);
-      });
+					return offerCoupons.docs.length === 0 || userCoupons.docs.length > 0;
+				})
+			).then((results) => {
+				offers.docs = offers.docs.filter((_, index) => results[index]);
+			});
 
-      if (offers.docs.length === 0) {
-        console.log(`[${slug}] - No offers available`);
-        return;
-      }
+			if (offers.docs.length === 0) {
+				console.log(`[${slug}] - No offers available`);
+				return;
+			}
 
-      const offer = offers.docs[
-        Math.floor(Math.random() * offers.docs.length)
-      ] as OfferIncluded;
+			const offer = offers.docs[
+				Math.floor(Math.random() * offers.docs.length)
+			] as OfferIncluded;
 
-      const { notificationSent, notificationInDb } = await sendPushNotification(
-        {
-          sub: user.notification_subscription,
-          payload,
-          userId: user.id,
-          payloadNotification: {
-            title: `${offer.partner.name} fait une offre 👀`,
-            message: `Une réduction ${offer.title} disponible sur l’appli carte “jeune engagé”`,
-            url: `${getBaseUrl()}/dashboard/offer/${offer.id}`,
-            slug,
-          },
-        }
-      );
+			const { notificationSent, notificationInDb } = await sendPushNotification(
+				{
+					sub: user.notification_subscription,
+					payload,
+					userId: user.id,
+					payloadNotification: {
+						title: `${offer.partner.name} fait une offre 👀`,
+						message: `Une réduction ${offer.title} disponible sur l’appli carte “jeune engagé”`,
+						url: `${getBaseUrl()}/dashboard/offer/${offer.source}/${offer.id}`,
+						slug,
+					},
+				}
+			);
 
-      if (notificationSent) nbOfNotificationsSent++;
-      if (notificationInDb) nbOfNotificationsInDb++;
-    }
+			if (notificationSent) nbOfNotificationsSent++;
+			if (notificationInDb) nbOfNotificationsInDb++;
+		}
 
-    console.log(`[${slug}] - Summary`, {
-      nbOfNotificationsSent,
-      nbOfNotificationsInDb,
-      slug,
-    });
-  } catch (e) {
-    console.log({
-      error: `[${slug}] - Error on condition to send notification`,
-    });
-    console.error(e);
-  }
+		console.log(`[${slug}] - Summary`, {
+			nbOfNotificationsSent,
+			nbOfNotificationsInDb,
+			slug,
+		});
+	} catch (e) {
+		console.log({
+			error: `[${slug}] - Error on condition to send notification`,
+		});
+		console.error(e);
+	}
 }
