6378e845f00ec581ba68914f6ad4e6eb2bf4af9e
Clément Lelong <clement@numericite.eu>
2024-11-11 13:38:48 +0100

    fix: offer image obiz

--- webapp/src/components/cards/ObizCard.tsx
+++ webapp/src/components/cards/ObizCard.tsx
@@ -1,4 +1,4 @@
-import { Text, Tag, Center, Link, Box } from "@chakra-ui/react";
+import { Text, Tag, Center, Link, Box, Flex } from "@chakra-ui/react";
 import { OfferIncluded } from "~/server/api/routers/offer";
 import { BarcodeIcon } from "../icons/barcode";
 import NextLink from "next/link";
@@ -27,26 +27,47 @@ export const ObizCard = (props: ObizCardProps) => {
         gap={2}
         p={4}
       >
-        <Box
-          borderRadius="2.5xl"
+        {offer.image && (
+          <Image
+            src={offer.image.url as string}
+            alt={offer.image.alt as string}
+            width={126}
+            height={70}
+            imageStyle={{
+              width: "126px",
+              height: "70px",
+              transform: "translateY(40%)",
+              marginTop: "-2rem",
+              zIndex: 0,
+            }}
+          />
+        )}
+        <Flex
+          alignItems="center"
+          borderRadius="lg"
           border="1px solid"
           borderColor="bgGray"
-          p={0.5}
+          p={1}
+          bg={"white"}
+          overflow={"hidden"}
+          zIndex={1}
+          height="35px"
+          maxWidth="80px"
         >
           <Image
             src={offer.partner.icon.url as string}
             alt={offer.partner.icon.alt as string}
-            width={40}
-            height={40}
+            width={offer.partner.icon.width || 80}
+            height={offer.partner.icon.height || 35}
             imageStyle={{
-              width: "40px",
-              height: "40px",
-              borderRadius: "20px",
+              width: "100%",
+              maxHeight: "32px",
+              borderRadius: "5px",
               objectFit: "cover",
               objectPosition: "center",
             }}
           />
-        </Box>
+        </Flex>
         <Tag fontWeight={700} w="fit-content" borderRadius="3xl">
           <BarcodeIcon mr={1} /> Bon d'achat
         </Tag>
