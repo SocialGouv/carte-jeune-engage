3c13d88dcd7f4fbdfed07fc4189508e9d269e1fb
Antoine Lelong <azenoxe@gmail.com>
2024-11-14 11:04:18 +0100

    fix: change second login from magic link to otp code in mail

--- webapp/src/components/landing/LoginOtpContent.tsx
+++ webapp/src/components/landing/LoginOtpContent.tsx
@@ -1,21 +1,11 @@
 import { useState } from "react";
 import OtpGenerated from "./OtpGenerated";
-import {
-  Box,
-  Flex,
-  Heading,
-  Icon,
-  ListItem,
-  Text,
-  UnorderedList,
-} from "@chakra-ui/react";
 import { api } from "~/utils/api";
 import { setCookie } from "cookies-next";
 import { useRouter } from "next/router";
 import BigLoader from "../BigLoader";
 import { SubmitHandler } from "react-hook-form";
 import { LoginForm } from "./PhoneNumberCTA";
-import { HiEnvelope } from "react-icons/hi2";
 
 type LoginOtpContentProps = {
   otpKind: "otp" | "email";
@@ -32,12 +22,6 @@ const LoginOtpContent = (props: LoginOtpContentProps) => {
   const [hasOtpError, setHasOtpError] = useState(false);
   const [hasOtpExpired, setHasOtpExpired] = useState(false);
 
-  const { data: resultSecretEmail } =
-    api.user.getSecretEmailFromPhoneNumber.useQuery({
-      phone_number: currentPhoneNumber,
-    });
-  const secretEmail = resultSecretEmail?.data.email;
-
   const { mutate: loginUser, isLoading: isLoadingLogin } =
     api.user.loginUser.useMutation({
       onSuccess: async ({ data }) => {
@@ -50,7 +34,7 @@ const LoginOtpContent = (props: LoginOtpContentProps) => {
         router.push("/dashboard");
       },
       onError: async ({ data }) => {
-        if (data?.httpStatus === 401) {
+        if (data?.httpStatus === 401 || data?.httpStatus === 404) {
           setHasOtpError(true);
         } else if (data?.httpStatus === 408) {
           setHasOtpExpired(true);
@@ -69,45 +53,10 @@ const LoginOtpContent = (props: LoginOtpContentProps) => {
 
   if (isLoading || isLoadingLogin) return <BigLoader />;
 
-  if (otpKind === "email") {
-    return (
-      <Flex flexDir="column" alignItems="center">
-        <Box borderRadius="2.5xl" bgColor="primary" p={5} position="relative">
-          <Icon as={HiEnvelope} color="white" w={7} h={7} mb={-1.5} />
-          <Box
-            position="absolute"
-            top={-1}
-            right={-1}
-            bgColor="white"
-            borderRadius="full"
-            p={1.25}
-          >
-            <Box bgColor="error" borderRadius="full" w={3.5} h={3.5} />
-          </Box>
-        </Box>
-        <Heading size="lg" fontWeight={800} textAlign="center" px={6} mt={10}>
-          Tout est dans notre dernier mail
-        </Heading>
-        <UnorderedList mt={10}>
-          <ListItem>
-            On vous a envoyé un mail à cette adresse :
-            <br />
-            <strong>{secretEmail}</strong>
-          </ListItem>
-          <ListItem mt={6}>Cliquez sur le lien dans le mail</ListItem>
-          <ListItem>Et voilà vous serez connecté !</ListItem>
-        </UnorderedList>
-        <Text color="disabled" fontSize={14} mt={10}>
-          Pensez à vérifier vos indésirables et spams si vous ne trouvez pas le
-          mail
-        </Text>
-      </Flex>
-    );
-  }
-
   return (
     <OtpGenerated
       currentPhoneNumber={currentPhoneNumber}
+      kind={otpKind}
       otp={otp}
       setOtp={setOtp}
       hasOtpError={hasOtpError}
--- webapp/src/components/landing/OtpGenerated.tsx
+++ webapp/src/components/landing/OtpGenerated.tsx
@@ -1,21 +1,30 @@
 import {
   Box,
+  Divider,
   Flex,
   Heading,
   HStack,
   Icon,
   Input,
+  Link,
   Text,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { HiChevronLeft } from "react-icons/hi2";
+import {
+  HiEnvelope,
+  HiMiniChatBubbleLeft,
+  HiMiniLockClosed,
+} from "react-icons/hi2";
 import OtpInput from "react-otp-input";
+import { api } from "~/utils/api";
 import { addSpaceToTwoCharacters } from "~/utils/tools";
+import NextLink from "next/link";
 
 const defaultTimeToResend = 30;
 
 type OtpGeneratedProps = {
   currentPhoneNumber: string;
+  kind: "otp" | "email";
   otp: string;
   setOtp: (value: string) => void;
   hasOtpError: boolean;
@@ -29,6 +38,7 @@ type OtpGeneratedProps = {
 const OtpGenerated = (props: OtpGeneratedProps) => {
   const {
     currentPhoneNumber,
+    kind,
     otp,
     setOtp,
     hasOtpError,
@@ -40,6 +50,17 @@ const OtpGenerated = (props: OtpGeneratedProps) => {
   } = props;
 
   const [timeToResend, setTimeToResend] = useState(defaultTimeToResend);
+  const [showHelpPage, setShowHelpPage] = useState(false);
+
+  const { data: resultSecretEmail } =
+    api.user.getSecretEmailFromPhoneNumber.useQuery(
+      {
+        phone_number: currentPhoneNumber,
+      },
+      { enabled: kind === "email" }
+    );
+
+  const secretEmail = resultSecretEmail?.data.email;
 
   const handleResendOtp = () => {
     handleGenerateOtp({ phone_number: currentPhoneNumber });
@@ -53,21 +74,99 @@ const OtpGenerated = (props: OtpGeneratedProps) => {
     return () => clearInterval(interval);
   }, [timeToResend]);
 
+  if (showHelpPage) {
+    return (
+      <Flex flexDir="column">
+        <Box alignSelf="center" borderRadius="2.5xl" position="relative">
+          <Icon as={HiMiniLockClosed} color="disabled" w={8} h={8} mb={-1.5} />
+        </Box>
+        <Heading
+          mt={6}
+          fontSize="2xl"
+          fontWeight="extrabold"
+          textAlign="center"
+        >
+          Vous n’arrivez pas à vous connecter à votre compte ?
+        </Heading>
+        <Text
+          mt={4}
+          fontSize={14}
+          fontWeight={500}
+          color="secondaryText"
+          textAlign="center"
+        >
+          Pour obtenir de l’aide si vous avez changé votre n° de téléphone ou
+          votre adresse email. Contactez-nous à l’adresse ci-dessous.
+        </Text>
+        <Link
+          as={NextLink}
+          href="mailto:cje@fabrique.social.gouv.fr"
+          fontSize={24}
+          fontWeight={800}
+          color="primary"
+          mt={6}
+          textDecor="underline"
+          textUnderlineOffset={2}
+          textDecorationThickness="3px"
+          passHref
+        >
+          cje@fabrique.social.gouv.fr
+        </Link>
+      </Flex>
+    );
+  }
+
   return (
     <>
       <Flex flexDir="column">
+        <Box alignSelf="center" borderRadius="2.5xl" position="relative" mb={6}>
+          <Icon
+            as={kind === "otp" ? HiMiniChatBubbleLeft : HiEnvelope}
+            color="primary"
+            w={8}
+            h={8}
+            mb={-1.5}
+          />
+          {kind === "email" && (
+            <Box
+              position="absolute"
+              top={0.5}
+              right={-0.5}
+              bgColor="white"
+              borderRadius="full"
+              p={0.5}
+            >
+              <Box bgColor="error" borderRadius="full" w={1.5} h={1.5} />
+            </Box>
+          )}
+        </Box>
         <Heading
           fontSize="2xl"
           fontWeight="extrabold"
           textAlign="center"
           mb={6}
         >
-          Vous avez reçu un code par SMS
+          Vous avez reçu un code par {kind === "otp" ? "SMS" : "mail"}
         </Heading>
-        <Text fontSize="sm" fontWeight="medium" color="secondaryText">
-          Saisissez le code envoyé au{" "}
-          {addSpaceToTwoCharacters(currentPhoneNumber)} pour pouvoir créer votre
-          compte
+        <Text
+          fontSize="sm"
+          fontWeight="medium"
+          color="secondaryText"
+          textAlign={kind == "email" ? "center" : "start"}
+        >
+          {kind === "otp" ? (
+            <>
+              Saisissez le code envoyé au{" "}
+              {addSpaceToTwoCharacters(currentPhoneNumber)} pour pouvoir créer
+              votre compte
+            </>
+          ) : (
+            <>
+              On vous a envoyé un mail à cette adresse :
+              <br />
+              {secretEmail}
+            </>
+          )}
         </Text>
         <Box my={8}>
           <HStack justifyContent="center">
@@ -111,7 +210,7 @@ const OtpGenerated = (props: OtpGeneratedProps) => {
           {hasOtpExpired && (
             <Text color="error" fontSize={"sm"} mt={2}>
               Le code n'est plus valide, cliquez sur le lien ci-dessous pour
-              recevoir un nouveau SMS
+              recevoir un nouveau {kind === "otp" ? "SMS" : "mail"}
             </Text>
           )}
           {hasOtpError && (
@@ -124,14 +223,34 @@ const OtpGenerated = (props: OtpGeneratedProps) => {
           mt={6}
           textDecor="underline"
           fontWeight="medium"
-          color={timeToResend <= 0 ? "black" : "gray.500"}
+          color={timeToResend <= 0 ? "blackLight" : "gray.500"}
           onClick={() => {
             if (timeToResend <= 0) handleResendOtp();
           }}
         >
-          Me renvoyer un code par SMS{" "}
+          Me renvoyer un code par {kind === "otp" ? "SMS" : "mail"}{" "}
           {timeToResend <= 0 ? "" : `(${timeToResend}s)`}
         </Text>
+        {kind === "email" && (
+          <>
+            <Divider my={6} />
+            <Text color="disabled" fontSize={14} fontStyle="italic">
+              Pensez à vérifier vos indésirables et spams si vous ne trouvez pas
+              le mail
+            </Text>
+            <Text
+              color="disabled"
+              fontWeight={700}
+              textDecoration="underline"
+              textDecorationThickness="2px"
+              textUnderlineOffset={2}
+              mt={2}
+              onClick={() => setShowHelpPage(true)}
+            >
+              Vous ne recevez pas du tout de mail ?
+            </Text>
+          </>
+        )}
       </Flex>
     </>
   );
