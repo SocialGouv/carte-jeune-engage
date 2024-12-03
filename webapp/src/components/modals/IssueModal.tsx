53057b52ee979d5b7ecc6ca6d6cf33729317414a
Antoine Lelong <azenoxe@gmail.com>
2024-12-03 11:04:43 +0100

    fix: modal layout and design

--- webapp/src/components/modals/IssueModal.tsx
+++ webapp/src/components/modals/IssueModal.tsx
@@ -123,10 +123,14 @@ const OrderIssueContent = ({
             <br />
             Que se passe-t-il ?
           </Text>
-          <RadioGroup display="flex" flexDir="column" gap={3.5} mt={8} px={4}>
+          <RadioGroup display="flex" flexDir="column" gap={3} mt={8}>
             {issues.map((cause) => (
               <Flex key={cause} alignItems="center">
-                <Radio value={cause} onChange={() => setSelectedCause(cause)}>
+                <Radio
+                  value={cause}
+                  onChange={() => setSelectedCause(cause)}
+                  gap={2}
+                >
                   <Text fontWeight={cause === selectedCause ? 800 : 500}>
                     {cause}
                   </Text>
@@ -199,57 +203,53 @@ const IssueModal = (props: IssueModalProps) => {
         ];
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="full">
+    <Modal isOpen={isOpen} onClose={onClose} isCentered={kind === "coupon"}>
       <ModalOverlay />
-      <ModalContent minH="full">
-        <ModalCloseButton />
-        <ModalBody display="flex" flexDir="column" px={8} pb={8} minH="full">
+      <ModalContent
+        mx={2.5}
+        mt={kind == "order" ? 5 : "auto"}
+        borderRadius="2.5xl"
+      >
+        <ModalCloseButton size="lg" top={4} right={4} />
+        <ModalBody display="flex" flexDir="column" px={9} pb={16} pt={4}>
           <OrderIssueContent kind={kind} id={id} issues={issues} />
           <Divider my={6} />
-          <Flex direction={"column"} gap={4} w="full">
-            <Flex direction={"column"} gap={4} mx={4}>
-              <ItemLink
-                onClick={() => setIsOpenCrisp(true)}
-                icon={HiMiniChatBubbleOvalLeftEllipsis}
-                text="Discutez avec nous en direct"
-              />
-              {kind === "order" && (
-                <>
-                  <Text
-                    my={2}
-                    fontSize={14}
-                    fontWeight={500}
-                    textAlign="center"
-                    color="disabled"
-                  >
-                    ou
+          <Flex direction="column" gap={4}>
+            <ItemLink
+              onClick={() => setIsOpenCrisp(true)}
+              icon={HiMiniChatBubbleOvalLeftEllipsis}
+              text="Discutez avec nous en direct"
+            />
+            {kind === "order" && (
+              <>
+                <Text
+                  my={2}
+                  fontSize={14}
+                  fontWeight={500}
+                  textAlign="center"
+                  color="disabled"
+                >
+                  ou
+                </Text>
+                <ItemLink
+                  href="telto:0472402828"
+                  icon={HiPhone}
+                  text="04 72 40 28 28"
+                />
+                <ItemLink
+                  href="mailto:serviceclient@reducce.fr"
+                  icon={HiEnvelope}
+                  text="serviceclient@reducce.fr"
+                />
+                <Flex direction="column" gap={4} fontSize={"sm"}>
+                  <Text fontWeight={500} textAlign="center" color="disabled">
+                    Disponible du lundi au vendredi de
+                    <br />
+                    09h à 12h30 puis de 14h à 17h30
                   </Text>
-                  <ItemLink
-                    href="telto:0472402828"
-                    icon={HiPhone}
-                    text="04 72 40 28 28"
-                  />
-                  <ItemLink
-                    href="mailto:serviceclient@reducce.fr"
-                    icon={HiEnvelope}
-                    text="serviceclient@reducce.fr"
-                  />
-                  <Flex
-                    direction="column"
-                    gap={4}
-                    fontSize={"sm"}
-                    mx={8}
-                    mt={3}
-                  >
-                    <Text textAlign="center" color="disabled">
-                      Disponible du lundi au vendredi de
-                      <br />
-                      09h à 12h30 puis de 14h à 17h30
-                    </Text>
-                  </Flex>
-                </>
-              )}
-            </Flex>
+                </Flex>
+              </>
+            )}
           </Flex>
         </ModalBody>
         {isOpenCrisp && user && (
